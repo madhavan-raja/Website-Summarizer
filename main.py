8a482936a9ffb55ded80baa64ec1d520455bd458
Madhavan <madhavanraja99@gmail.com>
2020-05-23 00:18:00 +0530

    Added virtual environment

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from flask import Flask, render_template, request
-from flask_cors import CORS
 import json
 
 import summarize
 
 error_message = "An error occoured while trying to reach the page."
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route('/main', methods=['POST'])
 def main():
-    print(request.method)
     data = request.data
     my_data = json.loads(data.decode("utf-8"))
     input_text = my_data['input_text']
@@ -23,6 +22,7 @@ def main():
     except:
         output = error_message
     return output
+
 
 if __name__ == '__main__':
     app.run(debug=True)
